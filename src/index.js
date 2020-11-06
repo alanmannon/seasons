5fc2dd55e782f109711f90b7870bbd6ca2ea6e93
Alan <alanmannon@gmail.com>
2020-11-05 19:36:05 -0600

    made App a class based method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hello</div>
-};
+class App extends React.Component {
+  return() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(
   <App />,
