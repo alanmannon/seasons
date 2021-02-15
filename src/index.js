1b472a403d78a3cb323383d85ccebf7b7218dca5
Alan <alanmannon@gmail.com>
2021-02-15 08:28:39 -0600

    added icons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -26,7 +20,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Lat: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading...</div>;
