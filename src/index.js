45f13f07e408aa296e3231512e69a775137b760d
Alan <alanmannon@gmail.com>
2021-02-15 09:26:31 -0600

    added loading spinner while app fetches user location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import "semantic-ui-css/semantic.min.css"
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
@@ -23,7 +25,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 }
 
